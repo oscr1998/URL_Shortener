22ee28398eab8007f924dc6587122d8eef447c27
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-11-30 14:21:14 +0000

    button connects to django

--- Clientside/src/Pages/Home/index.js
+++ Clientside/src/Pages/Home/index.js
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react";
+import API from "../../API";
+
 import React from 'react'
 import './style.css'
 
+
 export default function Home() {
+    const [orignalUrl, setOrignialUrl] = useState("");
+    const [shortUrl, setShortUrl] = useState("");
+    const [websites, setWebsites] = useState([]);
+
+    useEffect(() => {
+        refreshWebsites();
+        }, []);
+
+    const refreshWebsites = () => {
+        API.get("/").then((res) => {
+            setWebsites(res.data);
+            })
+        .catch(console.error);
+        };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        let item = { orignalUrl, shortUrl };
+        API.post("/", item).then(() => refreshWebsites());
+        };
+
     return (
             <div className='inputContainer'>
-                <form>
+                <form onSubmit={onSubmit}>
                     <label></label>
                     <input type="text" placeholder='Enter link here'></input>
                     <input type="submit" value="Shorten URL"></input>
